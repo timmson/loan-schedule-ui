7be0745c2a2616e7bfb154c3d199b79423c26969
Artem Krotov <timmson666@mail.ru>
2022-12-29 16:11:30 +0300

    Upgrade versions (#8)

--- src/form.tsx
+++ src/form.tsx
@@ -27,12 +27,12 @@ export default function Form(props: FormProps) {
 				<Field name={"paymentAmount"} value={(request.paymentAmount)} description={"Payment amount, $"} placeholder="Payment amount" onChange={(event) => change(event.target)}/>
 			</div>
 			<div className="row mt-2 text-left">
-				<Field name={"term"} value={request.term.toString()} description={"Term, months"} type="number" onChange={(event) => change(event.target)}/>
+				<Field name={"term"} value={request.term} description={"Term, months"} type="number" onChange={(event) => change(event.target)}/>
 				<Field name={"issueDate"} value={request.issueDate} description={"Issue date"} placeholder="dd.mm.yyyy" onChange={(event) => change(event.target)}/>
 			</div>
 			<div className="row mt-2 text-left border-bottom border-2 border-dark pb-2">
 				<Field name={"rate"} value={request.rate} description={"Annual rate, %"} type="number" step="0.01" onChange={(event) => change(event.target)}/>
-				<Field name={"paymentOnDay"} value={request.paymentOnDay.toString()} description={"Payment on day"} min="0" max="28" onChange={(event) => change(event.target)}/>
+				<Field name={"paymentOnDay"} value={request.paymentOnDay} description={"Payment on day"} min="0" max="28" onChange={(event) => change(event.target)}/>
 			</div>
 		</form>
 	)
--- test/test-early-modal.tsx
+++ test/test-early-modal.tsx
@@ -6,23 +6,24 @@ describe("EarlyModal should", () => {
 
 	let component = null
 
-	beforeAll(() => {
-		component = renderer.create(<EarlyModal
-			name={"name"} show={true}
-			ok={{name: "ok", action: () => null}}
-			close={{name: "close", action: () => null}}
-		>X</EarlyModal>
-		)
-	})
+	const createComponent = () => renderer.create(
+		<EarlyModal name={"name"} show={true} ok={{name: "ok", action: () => null}} close={{name: "close", action: () => null}}>
+            X
+		</EarlyModal>
+	)
 
 	/*	test("trigger close button", () => {
-		expect(component.toJSON()).toMatchSnapshot()
+		component = createComponent()
+		
+		act(() =>
+			component.root.			
+		)
 	})*/
 
 	test("equal to snapshot", () => {
+		component = createComponent()
+
 		expect(component.toJSON()).toMatchSnapshot()
 	})
-
-	afterAll(() => component.unmount())
 
 })
