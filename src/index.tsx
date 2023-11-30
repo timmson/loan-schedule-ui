45cbc81dea7f10f8a35ff84af3d3125437574b9f
timmson <timmson666@mail.ru>
2023-11-30 09:21:07 +0300

    - 2024 has been added

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import "bootstrap"
 import "./index.scss"
 
 import React from "react"
-import ReactDOM from "react-dom"
+import {createRoot} from "react-dom/client"
 import LoanSchedule from "loan-schedule.js"
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
 import {faTelegram} from "@fortawesome/free-brands-svg-icons"
@@ -19,17 +19,19 @@ const shareUrl = () => {
 	window.open(`https://t.me/share/url?url=${encodeURIComponent(window.location.href)}`, "blank")
 }
 
-ReactDOM.render(
+const container = document.getElementById("app")
+
+createRoot(container!).render(
 	<div className="container" style={{marginTop: 0, paddingTop: 0}}>
 		<App reducer={reducer}/>
 		<div className="row mt-3">
 			<div className="col text-end">
 				<a href="#" target="_blank" onClick={() => shareUrl()}>
-					[Share via <FontAwesomeIcon icon={faTelegram}/>]
+                    [Share via <FontAwesomeIcon icon={faTelegram}/>]
 				</a>
 				<p className="copyright">&copy; {currentYear} timmson</p>
 			</div>
 			<div className="col-sm-1">&nbsp;</div>
 		</div>
-	</div>,
-	document.getElementById("app"))
+	</div>
+)
